be6aa82504743ce4f4aaeefb5872f54837edbf04
NethmiRanasinghe <73777468+NethmiRanasinghe@users.noreply.github.com>
2022-06-18 20:54:15 +0530

    Update App.js

--- frontEnd/oral-cavity-image-capture/App.js
+++ frontEnd/oral-cavity-image-capture/App.js
@@ -2,7 +2,8 @@ import React from "react";
 
 import RegisterScreen from "./app/screens/RegisterScreen";
 import LoginScreen from "./app/screens/LoginScreen";
-
+import PatientRegisterScreen from "./app/screens/PatientRegisterScreen";
+import SelectPatientScreen from "./app/screens/SelectPatientScreen";
 
 export default function App() {
   return <LoginScreen /> ;
