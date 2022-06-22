82412eb10e766572b88e4d4703b0a19d7738d274
SRNethmi <e18282@eng.pdn.ac.lk>
2022-06-22 16:08:04 +0530

    add hash password in signup

--- backEnd/API/routes/UserAuthentication.js
+++ backEnd/API/routes/UserAuthentication.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const User = require("../models/User");
 const Request = require("../models/UserRequest");
+const bcrypt = require("bcrypt");
 
 /*************** post method to sign up a user to the database *****************************/
 
@@ -27,11 +28,15 @@ router.post("/signup", async (req, res) => {
         .json({ message: "This Registration Number already in use." });
     }
 
+    // encrypt the password - for security purposes
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
     const newUserReq = await Request({
       reg_no: req.body.reg_no,
       username: req.body.username,
       email: req.body.email,
-      password: req.body.password,
+      password: hashedPassword,  // store the encrypted password
     });
 
     const user = await newUserReq.save();
@@ -49,9 +54,13 @@ router.post("/signup", async (req, res) => {
 
 router.post("/login", async (req, res) => {
   try {
-    // check for the user inthe database
-    const user = await User.findOne({ email: req.body.email });
-    if (!user) return res.status(400).json({ message: "Wrong credentials!" });
+    // check whether the user has already signed up
+    const userByEmail = await User.findOne({ email: req.body.email });
+    if (!userByEmail) return res.status(400).json({ message: "Wrong credentials!" });
+
+    const userByPassword = await bcrypt.compare(req.body.password,user.password)
+    if(!userByPassword) return res.status(400).json({message:"Wrong credentials!"})
+
     res.status(200).json(user);
   } catch (error) {
     res.status(500).json(error);
