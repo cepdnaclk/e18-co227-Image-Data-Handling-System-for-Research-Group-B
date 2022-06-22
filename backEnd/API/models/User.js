7720d3738dba3880ad75d60d6e2a08d337b53b0c
SRNethmi <e18282@eng.pdn.ac.lk>
2022-06-22 09:29:06 +0530

    added user and user request models

---
+++ backEnd/API/models/User.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    username:{
+        type: String,
+        required: true,
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true
+
+    },
+    reg_no:{
+        type: String,
+        default: ""
+
+    },
+    password:{
+        type: String,
+        required: true,                                        
+    },
+    profile_pic:{
+        type: String,
+        required: false,
+        default:"",
+    },
+    role:{
+        type: Array,
+        required: false,  
+        default:[3],
+    }
+},
+// {
+//     timestamps:true
+// }
+);
+
+module.exports = mongoose.model("User",UserSchema)
---
+++ backEnd/API/models/UserRequest.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const RequestSchema = new mongoose.Schema({
+    username:{
+        type: String,
+        required: true,
+    },
+    email:{
+        type: String,
+        required: true,
+
+    },
+    reg_no:{
+        type: String,
+        required: true,
+        unique: true
+
+    },
+    password:{
+        type: String,
+        required: true,                                        
+    },
+},
+// {
+//     timestamps:true
+// }
+);
+
+module.exports = mongoose.model("Request",RequestSchema)
